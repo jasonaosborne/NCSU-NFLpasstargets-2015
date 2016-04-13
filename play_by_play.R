c4e6e57be316a10d7505aa8d72bdac9494f2bf29
gtpash <grahampash@hotmail.com>
2016-04-13 18:02:34 -0400

    You can finally pull everything
    This code should allow you to pull all of the data we wanted... for 2003
    at least...

--- play_by_play.R
+++ play_by_play.R
@@ -21,7 +21,10 @@ game_index <- function(year){
   return(playindex)
 }
 
-year <- 2004
+year <- 2003
+
+#initialize list to contain all of the results dataframes
+everythingYouEverWanted <- list()
 
 # Pull list of all games for the current year
 gamelist <- game_index(year)
@@ -55,6 +58,10 @@ for (i in 1:length(gamelist)) {
   #parse the play by play data
   #keep only rows that are 'passed' or 'sacked'
   keepRows <- c(grep('sack',PBP$Detail),grep('pass',PBP$Detail))
+  rmRows <- grep('Two Point',PBP$Detail)
+  if (length(rmRows)>0 & length(which(keepRows %in% rmRows))>0) {
+    keepRows <- keepRows[-which(keepRows %in% rmRows)] 
+  }
   plays <- PBP[keepRows[order(keepRows)],]
   rownames(plays) <- NULL
   
@@ -75,6 +82,8 @@ for (i in 1:length(gamelist)) {
                         PointDiff = numeric(nrow(plays)),
                         stringsAsFactors = F)
   
+  rm(PBP.raw,PBP,tableSource,url,whichRows,keepRows,tables)
+  
   for (j in 1:nrow(plays)) {
     detail <- plays$Detail[j]
     if (regexpr(' pass',detail) > 0) {
@@ -82,18 +91,23 @@ for (i in 1:length(gamelist)) {
       results$QB[j] <- substr(detail,start = 1,stop = regexpr(' pass',detail)-1)
       
       #getting target information
+      if (length(grep('incomplete',detail))>0 & length(grep('intended for',detail)) == 0) {
+        results$Target[j] <- ''
+      } else {
       targetPos <- ifelse(regexpr(' complete to ',detail) > 0,
                           regexpr(' complete to ',detail), 
                           regexpr(' intended for ',detail))
       
       temp <- strsplit(substr(detail,start = targetPos, stop = nchar(detail)), " ")
-      results$Target[j] <- paste(temp[[1]][4],temp[[1]][5])
+      temp <- paste(temp[[1]][4],temp[[1]][5])
+      results$Target[j] <- gsub("\\.","",temp)
       rm(targetPos,temp) # cleanup
+      }
       
       #getting yards
       temp <- strsplit(substr(detail,start=1,stop=(regexpr(' yards',detail)-1))," ")
       results$Yards[j] <- ifelse(length(temp[[1]])==0, 0, 
-                    as.numeric(temp[[1]][length(temp[[1]])]))
+                                 as.numeric(temp[[1]][length(temp[[1]])]))
       rm(temp)  # Cleanup
       
       results$Penalty[j] <- ifelse(length(grep('Penalty',detail))>0 & 
@@ -104,7 +118,7 @@ for (i in 1:length(gamelist)) {
       #getting yards
       temp <- strsplit(substr(detail,start=1,stop=(regexpr(' yards',detail)-1))," ")
       results$Yards[j] <- ifelse(length(temp[[1]])==0, 0, 
-                    as.numeric(temp[[1]][length(temp[[1]])]))
+                                 as.numeric(temp[[1]][length(temp[[1]])]))
       rm(temp)  # cleanup
     } else {
       warning('Play contains no pass/sack')
@@ -119,8 +133,47 @@ for (i in 1:length(gamelist)) {
     results$PointDiff[j] <- as.numeric(plays[j, which(colnames(plays)==homeTeam)]) - 
       as.numeric(plays[j, which(colnames(plays)== awayTeam)])
     
+    
+    # Pull in rosters to identify player designations
+    #load in home roster and rename
+    load(paste0("./rosters/",homeTeam,"_",year,"_roster.RData"))
+    homeRoster <- unique(playerMat)
+    #subset on QBs
+    homeQB <- subset(homeRoster,homeRoster[,1]=="QB")
+    #might as well remove playerMat
+    rm(playerMat)
+    load(paste0("./rosters/",awayTeam,"_",year,"_roster.RData"))
+    awayRoster <- unique(playerMat)
+    rm(playerMat)
+    
+    #QB lookup to assign possession
+    if (sum(grepl(results$QB[j],homeQB[,2]))==0) {
+      results$Poss[j] <- awayTeam
+    } else {
+      results$Poss[j] <- homeTeam
+    }
+    
+    #assign position to targetPos
+    if (results$Target[j] == '') {
+      results$TargetPos[j] <- ''
+    } else if (results$Poss[j] == results$Home[j]) {
+      results$TargetPos[j] <- homeRoster[grep(results$Target[j],homeRoster[,2])[1],1]
+      #not sure if it should be the first listed position
+    } else if (results$Poss[j] == results$Away[j]) {
+      results$TargetPos[j] <- awayRoster[grep(results$Target[j],awayRoster[,2])[1],1]
+    } else {
+      warning('Something is wrong')
+    }
+    
+    #we want field positon to be 0=home team's endzone, 100=away team's endzone
+    curToken <- strsplit(plays$Location[j],' ')
+    if (curToken[[1]][1] == results$Poss[j]) {
+      results$Location[j] <- as.numeric(curToken[[1]][2])
+    } else {
+      results$Location[j] <- 100 - as.numeric(curToken[[1]][2])
+    }
+    #get rid of the token
+    rm(curToken)
   }
-
-  
-  # Pull in rosters to identify player designations
+  everythingYouEverWanted[[i]] <- results
 }
